a9ca9e39257913743bd0e936acbbdd0902c00519
Axel Magnuson <axelmagn@gmail.com>
2013-07-24 16:29:35 -0600

    set up static serving.  copy over static and template folders from twoscoops template

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -97,7 +97,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'static'))
+    normpath(join(SITE_ROOT, 'static')),
 )
 
 # List of finder classes that know how to find static files in
