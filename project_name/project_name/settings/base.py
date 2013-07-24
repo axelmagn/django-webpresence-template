c7ebdb869b5c1b06b304257b8d7b47e62de14a9f
Axel Magnuson <axelmagn@gmail.com>
2013-07-24 16:23:15 -0600

    add django-cms reqs

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -89,7 +89,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = normpath(join(SITE_ROOT,'assets'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -97,9 +97,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    normpath(join(SITE_ROOT, 'static'))
 )
 
 # List of finder classes that know how to find static files in
